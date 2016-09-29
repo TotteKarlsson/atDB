20589ba9e4de46953016e538d45477916dcbcbf5
Totte Karlsson <v-matsk@alleninstitute.org>
2016-09-29 10:18:09 -0700

    Moved all files to arraybot repository

--- mysql/dump.sql
+++ mysql/dump.sql
@@ -1,5 +1,5 @@
 /*
-SQLyog Trial v12.2.6 (64 bit)
+SQLyog Ultimate v12.18 (64 bit)
 MySQL - 5.7.15-log : Database - atdb
 *********************************************************************
 */
@@ -23,14 +23,18 @@ DROP TABLE IF EXISTS `block`;
 CREATE TABLE `block` (
   `id` int(20) NOT NULL AUTO_INCREMENT,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` tinytext NOT NULL,
+  `created_by` int(11) NOT NULL,
   `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` int(11) NOT NULL DEFAULT '0',
   `label` tinytext,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 
 /*Data for the table `block` */
+
+insert  into `block`(`id`,`created`,`created_by`,`modified`,`status`,`label`) values 
+(28,'2016-09-27 17:04:05',21,'2016-09-27 17:04:05',0,'adfadfa asdf'),
+(29,'2016-09-27 17:04:43',21,'2016-09-27 17:04:43',0,'adf a df adsf');
 
 /*Table structure for table `block_note` */
 
@@ -47,6 +51,11 @@ CREATE TABLE `block_note` (
 
 /*Data for the table `block_note` */
 
+insert  into `block_note`(`block_id`,`note_id`) values 
+(28,24),
+(29,25),
+(28,26);
+
 /*Table structure for table `note` */
 
 DROP TABLE IF EXISTS `note`;
@@ -57,9 +66,14 @@ CREATE TABLE `note` (
   `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `created_by` tinytext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 
 /*Data for the table `note` */
+
+insert  into `note`(`id`,`note`,`created_on`,`created_by`) values 
+(24,' asdf\nsdafsd asdf \nasdf adsfadfasdfasd','2016-09-27 17:04:05','21'),
+(25,' dfa sdfadfadfa','2016-09-27 17:04:43','21'),
+(26,'','2016-09-27 17:07:08','7');
 
 /*Table structure for table `ribbon` */
 
@@ -99,14 +113,18 @@ CREATE TABLE `ribbon_note` (
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_name` (`user_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user` */
+
+insert  into `user`(`id`,`user_name`,`created`) values 
+(7,'Totte','2016-09-22 14:21:12'),
+(21,'test','2016-09-23 16:54:11');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
