f8e9bb8b3b88df62ff3d60513fee4d70e24eac30
Totte Karlsson <tottek@gmail.com>
2018-03-21 16:52:59 -0700

    Fixing redistribution

--- source/postbuild_bcc32.sh
+++ source/postbuild_bcc32.sh
@@ -37,41 +37,41 @@ cp $OUTPUTDIR/$OUTPUTFILENAME $dest
  cp $OUTPUTDIR/atBarcodeReader.dll              $dest
  cp $OUTPUTDIR/atSSI.dll                        $dest
 
- cp $BDSCOMMONDIR/BPL/atVCLCore.bpl             $dest    
- cp $BDSCOMMONDIR/BPL/DuneComponentsDD.bpl      $dest
- cp $BDSCOMMONDIR/BPL/DuneFormsDD.bpl           $dest
- cp $BDSCOMMONDIR/BPL/VCLCommonDD.bpl           $dest
+ cp $BDSCOMMONDIR/BPL/atVCLCoreBCC32D.bpl             $dest    
+ cp $BDSCOMMONDIR/BPL/DuneComponentsBCC32D.bpl      $dest
+ cp $BDSCOMMONDIR/BPL/DuneFormsBCC32D.bpl           $dest
+ cp $BDSCOMMONDIR/BPL/VCLCommonBCC32D.bpl           $dest
 
 #Borland Packages and dll's
-BC_REDIST="/cygdrive/c/CodeGear/Tokyo/bin"
+CG_REDIST="/cygdrive/c/CodeGear/Tokyo/bin"
 
-cp $BC_REDIST/borlndmm.dll                      $dest
-cp $BC_REDIST/cc32250mt.dll                     $dest
-cp $BC_REDIST/midas.dll                         $dest
+cp $CG_REDIST/borlndmm.dll                      $dest
+cp $CG_REDIST/cc32250mt.dll                     $dest
+cp $CG_REDIST/midas.dll                         $dest
 
 suffix="250.bpl"
-cp $BC_REDIST/bindcomp$suffix                   $dest
-cp $BC_REDIST/bindcompvcl$suffix                $dest
-cp $BC_REDIST/bindengine$suffix                 $dest
-cp $BC_REDIST/dbexpress$suffix                  $dest
-cp $BC_REDIST/dbrtl$suffix                      $dest
-cp $BC_REDIST/dbxcds$suffix                     $dest
-cp $BC_REDIST/DbxCommonDriver$suffix		    $dest
-cp $BC_REDIST/DbxDevartPostgreSQLDriver$suffix	$dest
-cp $BC_REDIST/DBXMySQLDriver$suffix		        $dest
-cp $BC_REDIST/dsnap$suffix                      $dest
-cp $BC_REDIST/rtl$suffix                        $dest
-cp $BC_REDIST/vcl$suffix                        $dest
-cp $BC_REDIST/vclactnband$suffix                $dest
-cp $BC_REDIST/vcldb$suffix                      $dest
-cp $BC_REDIST/vclimg$suffix                     $dest
-cp $BC_REDIST/vclx$suffix                       $dest
+cp $CG_REDIST/bindcomp$suffix                   $dest
+cp $CG_REDIST/bindcompvcl$suffix                $dest
+cp $CG_REDIST/bindengine$suffix                 $dest
+cp $CG_REDIST/dbexpress$suffix                  $dest
+cp $CG_REDIST/dbrtl$suffix                      $dest
+cp $CG_REDIST/dbxcds$suffix                     $dest
+cp $CG_REDIST/DbxCommonDriver$suffix		    $dest
+cp $CG_REDIST/DbxDevartPostgreSQLDriver$suffix	$dest
+cp $CG_REDIST/DBXMySQLDriver$suffix		        $dest
+cp $CG_REDIST/dsnap$suffix                      $dest
+cp $CG_REDIST/rtl$suffix                        $dest
+cp $CG_REDIST/vcl$suffix                        $dest
+cp $CG_REDIST/vclactnband$suffix                $dest
+cp $CG_REDIST/vcldb$suffix                      $dest
+cp $CG_REDIST/vclimg$suffix                     $dest
+cp $CG_REDIST/vclx$suffix                       $dest
 
-miscRedist="/cygdrive/p/redist/atdb"
+miscRedist="/cygdrive/p/redist"
 cp $miscRedist/dbexppgsql40.dll             $dest
 cp $miscRedist/tsclib.dll                   $dest
 cp $miscRedist/navusbapi.dll                $dest
 
 #for local bin folder
 
-cp $miscRedist/navusbapi.dll                $OUTPUTDIR
+#cp $miscRedist/navusbapi.dll                $OUTPUTDIR
