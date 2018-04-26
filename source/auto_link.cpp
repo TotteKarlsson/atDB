e695f54c7b4d81ccf8131afc8246b85a35dc1abf
Totte Karlsson <tottek@gmail.com>
2018-04-25 17:40:26 -0700

    Updated linking

--- source/auto_link.cpp
+++ source/auto_link.cpp
@@ -1,15 +1,14 @@
-#pragma comment(lib, "dslFoundation.lib")
+#pragma comment(lib, "dslCommon.lib")
 #pragma comment(lib, "dslVCLCommon.lib")
 #pragma comment(lib, "dslVCLComponents.bpi")
-#pragma comment(lib, "dslVisuals.bpi")
-
+#pragma comment(lib, "dslVCLVisualComponents.bpi")
 
 #pragma comment(lib, "poco_foundation-static.lib")
 
-#pragma comment(lib, "atCore.lib")
-#pragma comment(lib, "atDatabase.lib")
-#pragma comment(lib, "atSSI.lib")
-
+//#pragma comment(lib, "atCore.lib")
+//#pragma comment(lib, "atDatabase.lib")
+#pragma comment(lib, "atBarcodeReader.lib")
+#pragma comment(lib, "atFoundation.lib")
 
 #pragma comment(lib, "DBXDevartPostgreSQLDriver250.bpi")
 
